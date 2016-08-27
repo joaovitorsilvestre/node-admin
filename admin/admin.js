b6a89b68fd2ece6bdd8ace76d52cabc34f336faf
João <joao@localhost.localdomain>
2016-08-27 17:41:58 -0300

    fix admin importing model that doesnt exists and update gitignore

--- admin/admin.js
+++ admin/admin.js
@@ -1,11 +1,8 @@
 var User = require('../models/user');
-var Pessoa = require('../models/pessoa');
 
 var userModel = User.model; // get the model compiled
-var pessoaModel = Pessoa.model; // get the model compiled
 
 // register the module to be displaied in admin
 module.exports = {
     user: userModel,
-    pessoa: pessoaModel
 }
